4e5f930cd5a37719ccce550bce1393777f639454
Benedikt Böhm <bb@xnull.de>
2011-07-26 11:31:56 +0200

    multistage: do not support a default stage

--- lib/capper/multistage.rb
+++ lib/capper/multistage.rb
@@ -1,25 +1,17 @@
 require File.dirname(__FILE__) + '/../capper' unless defined?(Capper)
 
 Capper.load do
+  # execute the specified stage so that recipes required in stage can contribute to task list
   on :load do
     if stages.include?(ARGV.first)
-      # Execute the specified stage so that recipes required in stage can contribute to task list
       find_and_execute_task(ARGV.first) if ARGV.any?{ |option| option =~ /-T|--tasks|-e|--explain/ }
-    else
-      # Execute the default stage so that recipes required in stage can contribute tasks
-      find_and_execute_task(default_stage) if exists?(:default_stage)
     end
   end
 
   namespace :multistage do
     task :ensure do
-      if !exists?(:stage)
-        if exists?(:default_stage)
-          logger.important "Defaulting to `#{default_stage}'"
-          find_and_execute_task(default_stage)
-        else
-          abort "No stage specified. Please specify one of: #{stages.join(', ')} (e.g. `cap #{stages.first} #{ARGV.last}')"
-        end
+      unless exists?(:stage)
+        abort "No stage specified. Please specify one of: #{stages.join(', ')} (e.g. `cap #{stages.first} #{ARGV.last}')"
       end
     end
   end
