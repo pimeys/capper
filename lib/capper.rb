895bacbd2010f9caeaa84bdeacc81a106d019cfd
Benedikt Böhm <bb@xnull.de>
2011-07-26 13:12:00 +0200

    overwrite deploy:setup to get rid of stupid group-writable homedir

--- lib/capper.rb
+++ lib/capper.rb
@@ -28,4 +28,24 @@ Capper.load do
 
   # cleanup by default
   after "deploy:update", "deploy:cleanup"
+
+  namespace :deploy do
+    desc <<-DESC
+      Prepares one or more servers for deployment. Before you can use any \
+      of the Capistrano deployment tasks with your project, you will need to \
+      make sure all of your servers have been prepared with `cap deploy:setup'. When \
+      you add a new server to your cluster, you can easily run the setup task \
+      on just that server by specifying the HOSTS environment variable:
+
+        $ cap HOSTS=new.server.com deploy:setup
+
+      It is safe to run this task on servers that have already been set up; it \
+      will not destroy any deployed revisions or data.
+    DESC
+    task :setup, :except => { :no_release => true } do
+      dirs = [releases_path, shared_path]
+      dirs += shared_children.map { |d| File.join(shared_path, d) }
+      run "mkdir -p #{dirs.join(' ')}"
+    end
+  end
 end
