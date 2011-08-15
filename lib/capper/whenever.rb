0e33daec65697d4cd412f51666370676374eb17c
Benedikt Böhm <bb@xnull.de>
2011-08-15 18:41:37 +0200

    really clear crontab and do not leave stray whenever lines around

--- lib/capper/whenever.rb
+++ lib/capper/whenever.rb
@@ -35,24 +35,15 @@ Capper.load do
         if previous_release
           run "cd #{previous_release} && #{whenever_command} #{whenever_update_flags}"
         else
-          run "cd #{release_path} && #{whenever_command} #{whenever_clear_flags}"
+          run "crontab /dev/null"
         end
       end
 
       run "cd #{current_path} && #{whenever_command} #{whenever_update_flags}"
     end
 
-    desc <<-DESC
-      Clear application's crontab entries using Whenever. You can configure \
-      the command used to invoke Whenever by setting the :whenever_command \
-      variable, which can be used with Bundler to set the command to \
-      "bundle exec whenever". You can configure the identifier used by setting \
-      the :whenever_identifier variable, which defaults to the same value configured \
-      for the :application variable. Finally, you can completely override all \
-      arguments to the Whenever command by setting the :whenever_clear_flags variable.
-    DESC
     task :clear_crontab do
-      run "cd #{release_path} && #{whenever_command} #{whenever_clear_flags}"
+      run "crontab /dev/null"
     end
   end
 end
